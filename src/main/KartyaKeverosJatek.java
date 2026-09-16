710f07f8dbe03ee6e0defd699ad29c2a4eb8936d
KovácsTímeaBarbara(S <KovcsTmeaBarbara(S@192.168.228.16>
2026-09-16 16:57:14 +0200

    feltoltes() felkesz

--- src/main/KartyaKeverosJatek.java
+++ src/main/KartyaKeverosJatek.java
@@ -1,26 +1,36 @@
 package main;
 
 public class KartyaKeverosJatek {
-
+    
+    static String[] pakli = new String[22];
+    
     public static void main(String[] args) {
-        String[] szinek = {"P", "T", "Z", "M"};
-        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
         
+        feltolt();
         for (int i = 0; i < 3; i++) {
             kirak();//1 tömb
             melyik();//Scanner
-            kever();//kever
+            kever();//középre
+        }
+        ezVolt();//11. azaz középső lap
+    }
+
+    private static void feltolt() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int db = 0;
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+                
+            }
         }
     }
 
-    private static void kirak() {
-        
-    }
-    private static void melyik() {
-        
-    }
-    private static void kever() {
-        
-    }
+    private static void kirak() {}
     
+    private static void melyik() {}
+    
+    private static void kever() {}
+    
+    private static void ezVolt() {}
 }
