e8959e4326721497128dc50768d3d0d9c9b0e78f
KovácsTímeaBarbara(S <KovcsTmeaBarbara(S@192.168.228.16>
2026-09-16 16:30:00 +0200

    alap

---
+++ src/main/KartyaKeverosJatek.java
@@ -0,0 +1,26 @@
+package main;
+
+public class KartyaKeverosJatek {
+
+    public static void main(String[] args) {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        
+        for (int i = 0; i < 3; i++) {
+            kirak();//1 tömb
+            melyik();//Scanner
+            kever();//kever
+        }
+    }
+
+    private static void kirak() {
+        
+    }
+    private static void melyik() {
+        
+    }
+    private static void kever() {
+        
+    }
+    
+}
