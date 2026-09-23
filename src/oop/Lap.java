ee040de1addfcfb54226cb7f2c91e0654fc657bd
KovácsTímeaBarbara(S <KovcsTmeaBarbara(S@192.168.228.16>
2026-09-23 16:45:12 +0200

    Lap osztaly

--- src/oop/Lap.java
+++ src/oop/Lap.java
@@ -1,5 +1,22 @@
 package oop;
 
 public class Lap {
+    private String ertek;
+    private String szin;
+
+    public Lap(String ertek, String szin) {
+        this.ertek = ertek;
+        this.szin = szin;
+    }
+
+    public String getErtek() {
+        return ertek;
+    }
+    public String getSzin() {
+        return szin;
+    }
     
+    public String megjelenit() {
+        return "Lap{" + "ertek=" + ertek + ", szin=" + szin + '}';
+    }
 }
